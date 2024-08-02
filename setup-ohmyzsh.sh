69829f792e563b389b3bb5558fc96680e9bcde33
Chomba <dev.chomba@gmail.com>
2024-08-02 16:46:31 +0200

    initialised

---
+++ setup-ohmyzsh.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Update and install required packages
+sudo yum update -y
+sudo yum install -y zsh git
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install Zsh plugins for autosuggestions and syntax highlighting
+ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+# Update .zshrc to include the plugins
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
+
+# Apply changes
+source ~/.zshrc
+
+echo "Oh My Zsh with autocomplete and syntax highlighting has been installed and configured!"
+
